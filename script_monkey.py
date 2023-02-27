2944d48e042270382f7221afa58b4cc414028a7d
Lars Erik Wik <lars.erik.wik@northern.tech>
2023-02-27 18:18:54 +0100

    Add script monkey
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- script_monkey.py
+++ script_monkey.py
@@ -11,34 +11,35 @@ def call_maybe(probability):
         return inner
     return decorator
 
-@call_maybe(100)
+@call_maybe(25)
 def monkey_cron():
     commands = [
-        ["chown", "nobody", "/etc/cron.allow"],
-        ["chgrp", "nobody", "/etc/cron.allow"],
+        ["chown", "script_monkey", "/etc/cron.allow"],
+        ["chgrp", "script_monkey", "/etc/cron.allow"],
         ["chmod", "0777", "/etc/cron.allow"],
-        ["chown", "nobody", "/etc/crontab"],
-        ["chgrp", "nobody", "/etc/crontab"],
+        ["chown", "script_monkey", "/etc/crontab"],
+        ["chgrp", "script_monkey", "/etc/crontab"],
         ["chmod", "0777", "/etc/crontab"],
-        ["chown", "nobody", "/etc/cron.d"],
-        ["chgrp", "nobody", "/etc/cron.d"],
+        ["chown", "script_monkey", "/etc/cron.d"],
+        ["chgrp", "script_monkey", "/etc/cron.d"],
         ["chmod", "0777", "/etc/cron.d"],
-        ["chown", "nobody", "/etc/cron.hourly"],
-        ["chgrp", "nobody", "/etc/cron.hourly"],
+        ["chown", "script_monkey", "/etc/cron.hourly"],
+        ["chgrp", "script_monkey", "/etc/cron.hourly"],
         ["chmod", "0777", "/etc/cron.hourly"],
-        ["chown", "nobody", "/etc/cron.daily"],
-        ["chgrp", "nobody", "/etc/cron.daily"],
+        ["chown", "script_monkey", "/etc/cron.daily"],
+        ["chgrp", "script_monkey", "/etc/cron.daily"],
         ["chmod", "0777", "/etc/cron.daily"],
-        ["chown", "nobody", "/etc/cron.weekly"],
-        ["chgrp", "nobody", "/etc/cron.weekly"],
+        ["chown", "script_monkey", "/etc/cron.weekly"],
+        ["chgrp", "script_monkey", "/etc/cron.weekly"],
         ["chmod", "0777", "/etc/cron.weekly"],
-        ["chown", "nobody", "/etc/cron.monthly"],
-        ["chgrp", "nobody", "/etc/cron.monthly"],
+        ["chown", "script_monkey", "/etc/cron.monthly"],
+        ["chgrp", "script_monkey", "/etc/cron.monthly"],
         ["chmod", "0777", "/etc/cron.monthly"],
     ]
     commands = list(filter(lambda x: os.path.exists(x[2]), commands))
-    commands = random.sample(commands, random.randrange(0, len(commands)))
+    commands = random.sample(commands, random.randrange(1, len(commands)))
     for command in commands:
+        print("Running command '%s'" % " ".join(command))
         subprocess.run(command)
 
 
