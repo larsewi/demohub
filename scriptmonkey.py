ba7a2ec4b949db54e8e04951f653efb8ee4fe036
Lars Erik Wik <lars.erik.wik@northern.tech>
2023-02-27 17:08:58 +0100

    Added module './script_monkey.cf'

---
+++ scriptmonkey.py
@@ -0,0 +1,28 @@
+
+def main():
+    # Change perms and owner on these files
+    # sudo chown root /etc/cron.allow
+    # sudo chgrp root /etc/cron.allow
+    # sudo chmod 0640 /etc/cron.allow
+    # sudo chown root /etc/crontab
+    # sudo chgrp root /etc/crontab
+    # sudo chmod 0600 /etc/crontab
+    # sudo chown root /etc/cron.d
+    # sudo chgrp root /etc/cron.d
+    # sudo chmod 0700 /etc/cron.d
+    # sudo chown root /etc/cron.hourly
+    # sudo chgrp root /etc/cron.hourly
+    # sudo chmod 0700 /etc/cron.hourly
+    # sudo chown root /etc/cron.daily
+    # sudo chgrp root /etc/cron.daily
+    # sudo chmod 0700 /etc/cron.daily
+    # sudo chown root /etc/cron.weekly
+    # sudo chgrp root /etc/cron.weekly
+    # sudo chmod 0700 /etc/cron.weekly
+    # sudo chown root /etc/cron.monthly
+    # sudo chgrp root /etc/cron.monthly
+    # sudo chmod 0700 /etc/cron.monthly
+    pass
+
+if __name__ == "__main__":
+    main()
